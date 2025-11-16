66f79da4256bdbe7b48744556c615e7e52510b07
soum <hugo.soum-brugevin@efrei.net>
2025-11-16 03:44:43 +0100

    fix: helper function for modals

--- desktop/src/lib/actions/clickOutside.ts
+++ desktop/src/lib/actions/clickOutside.ts
@@ -1,12 +1,11 @@
 export function clickOutside(node: HTMLElement, callback: () => void) {
   function handleClick(event: MouseEvent) {
     if (!node.contains(event.target as Node)) {
-        console.log("Clicked outside");
       callback();
     }
   }
 
-  document.addEventListener("click", handleClick, true);
+  document.addEventListener("click", handleClick);
 
   return {
     destroy() {
