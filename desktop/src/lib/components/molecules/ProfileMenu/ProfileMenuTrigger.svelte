fb84ff14c4ceab997bb46e499f70b4b913ccd06c
soum <hugo.soum-brugevin@efrei.net>
2025-11-16 03:45:33 +0100

    add new Chevron svg

--- desktop/src/lib/components/molecules/ProfileMenu/ProfileMenuTrigger.svelte
+++ desktop/src/lib/components/molecules/ProfileMenu/ProfileMenuTrigger.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
   import Avatar from '$lib/components/atoms/Avatar.svelte';
   import ChevronDown from '$lib/components/icons/ChevronDown.svelte';
+  import ChevronUp from '$lib/components/icons/ChevronUp.svelte';
   export let username = "";
   export let avatar = "";
   export let onClick = () => {};
+  export let show = false;
 </script>
 
-<div class="flex items-center gap-2 cursor-pointer select-none bg-none border-none">
+<div class="flex items-center gap-2 select-none bg-none border-none">
   <Avatar src={avatar} size={48} />
   <span class="p-1 text-black font-bold text-xl">{username}</span>
 
 <button
   class="p-1 hover:bg-gray-100 rounded-lg transition"
-  on:click={onClick}
+  on:click|stopPropagation={onClick}
 >
+    {#if !show}
   <ChevronDown />
+  {:else}
+  <ChevronUp />
+  {/if}
 </button>
 </div>
